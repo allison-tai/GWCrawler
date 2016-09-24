ba1304f39b2304f6bf6f26b060212dc287cb73df
l0f8 <allison.tai@alumni.ubc.ca>
2016-09-24 16:22:53 -0700

    Messing with Jsoup

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,18 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
     public static void main( String[] args )
     {
 
         System.out.println("YAY");
-
+        try {
+            Crawler.processPage("http://www.mit.edu/");
+        }
+        catch (IOException e) {
+        }
         try {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setServiceAccount(new FileInputStream("path/to/serviceAccountCredentials.json"))
