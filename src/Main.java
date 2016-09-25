c0ca482f86f75dc9183ba6fbaf406332b407fc8e
Lily Guo <lily2guo@gmail.com>
2016-09-24 21:19:12 -0700

    Merge branch 'master' of https://github.com/faelicy/GWCrawler
    Conflicts:
    	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,33 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.*;
 
-import javax.xml.crypto.Data;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main( String[] args )
     {
 
-        System.out.println("YAY");
-
-        // TODO: Populate the Bestbuy list
-
-        // TODO: Populate the Steam list
-
-        // TODO: Populate the game list using combination of Bestbuy and Steam?
-
         try {
-            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/playstation/621715.aspx");
+            List<Game> games = new LinkedList<Game>();
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/playstation-4-games/33934.aspx?path=f7745ff8832df8fd7aa9509ae18c8934en01", games, "PS4");
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/playstation-3/24334.aspx?path=97b55ed0c4ce4600a8ae66201e9d8a7fen01", games, "PS3");
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/nintendo-wii-u-games/32276.aspx?path=c2e0396bf4fabe11f83eee8998743759en01", games, "WiiU");
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/nintendo-wii-games/24336.aspx?path=fdd5fd017035e25b90dbe5c1b3ecf623en01", games, "Wii");
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/nintendo-handheld/621901.aspx?path=5aca53d7b7bff9cd44b0c3588418d6b8en01", games, "3DS");
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/nintendo-ds-games/22026.aspx?path=b3bec2624c13240ee715674a36bcce48en01", games, "DS");
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/xbox-one-games/35511.aspx?path=44a462b3cfb1dace048bd57666031870en01", games, "XBoxOne");
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/xbox-360/23393.aspx?path=002f08194a4d0cb1d05cb0c739e6262ben01", games, "XBox360");
+            Crawler.processPage("http://www.bestbuy.ca/en-CA/category/pc-games/21136.aspx?path=986beba798dde00c1554f6bae338c9d3en01", games, "PC");
 
             Crawler.processPageSteam("http://store.steampowered.com/search/?sort_by=Reviews_DESC&category1=998&os=win");
         }
+        // TODO: Populate the Steam list
+        // TODO: Populate the game list using combination of Bestbuy and Steam?
+
         catch (IOException e) {
         }
 
